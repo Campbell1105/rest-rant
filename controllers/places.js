e4749057268cdc7b787fa214ae958bfca7561531
Campbell1105 <135083465+Campbell1105@users.noreply.github.com>
2023-08-27 10:42:05 -0400

    part 6

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,31 @@
 const router = require('express').Router()
 
-router.get('/', (req, res) => {
-    res.send('GET /places')
+router.get('/:id', (req, res) => {
+    let id= Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+    res.render('places/show', { place:places[id], id})
+    }
 })
+
+router.delete('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+      places.splice(id,1)
+      res.redirect('/places')
+    }
+  })
+  
 
 module.exports = router
